30b95d0d6cd0ef4c4cde1467781ec6f1263ad3de
Trieu <trieuhaipham32621998@gmail.com>
2024-08-24 19:53:49 +0700

    :sparkles: [products] Create CRUD

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+	Body,
+	Controller, Delete, Get, Param, Patch, Post
+} from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+	/*
+	REMEMBER: Route order is important
+	GET		/products
+	POST	/products
+	GET		/products/old_products	Static route => Has to be writen before dynamic route, otherwise it will go into the :id route instead
+	GET		/products/:id			| These are dynamics route.
+	PATCH	/products/:id			| Any string after "/products" will be accepted (if they use the same request type "GET", "POST", etc.),
+	DELETE	/products/:id			| and static route written below won't work
+	*/
+
+	@Get()		// GET /products
+	findManyProducts() {
+
+		return [];
+	}
+	
+	@Get(":id")	//Get products/:id
+	findProduct(@Param("id") id:string) {
+
+		return {};
+	}
+	
+	@Post("")	//POST products
+	createProduct(@Body() product: {}) {
+
+		return {};
+	}
+
+	@Patch("id")	//PATCH products/:id
+	updateProduct(@Param("id") id: string, @Body() productUpdate: {}) {
+
+		return productUpdate;
+	}
+	
+	@Delete(":id")	//Get products/:id
+	deleteProduct(@Param("id") id:string) {
+
+		return {};
+	}
+}
