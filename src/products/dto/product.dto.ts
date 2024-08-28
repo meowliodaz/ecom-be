63a6d81476e1bdd3b10075113cece8383fe8391a
Trieu <trieuhaipham32621998@gmail.com>
2024-08-28 09:38:39 +0700

    :zap: [product] Add data validation to DTO

--- src/products/dto/product.dto.ts
+++ src/products/dto/product.dto.ts
@@ -1,17 +1,45 @@
 import { PartialType } from "@nestjs/mapped-types";
+import { IsDate, IsDecimal, IsEnum, IsInt, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateProductDto {
+	@IsNotEmpty()
+	@IsString()
 	name: string;
+
+	@IsNotEmpty()
+	@IsString()
 	author: string;
+	
+	@IsString()
 	publisher: string;
+	
+	@IsString()
 	description: string;
-	category: string;
+
+	@IsNotEmpty()
+	@IsEnum(["pdf", "epub"], {
+		message: "Invalid role! Please insert only \"pdf\" or \"epub\"."
+	})
 	format: "pdf" | "epub";
+	
+	@IsNotEmpty()
+	@IsDecimal()
 	price: number;
+	
+	@IsDecimal()
 	salePrice: number;
+	
+	@IsNotEmpty()
+	@IsInt()
 	stocks: number;
+	
+	@IsString()
 	image: string;
+	
+	@IsDate()
 	created_at: Date;
+
+	@IsDate()
 	updated_at: Date;
 }
 
